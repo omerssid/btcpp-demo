fe6a7302d7a7227bfcbe32d3567cdf0c115657c4
omerssid <omarh9323@gmail.com>
2024-02-02 00:16:58 +0300

    First demo

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "behaviortree_cpp/bt_factory.h"
+// file that contains the custom nodes definitions
+#include "my_nodes.h"
+using namespace MyNodes;
+
+using BT::BehaviorTreeFactory;
+using BT::TreeNode;
+
+/*
+There are 3 ways to create a node:
+    1. Node Class (inheritance)
+    2. Function Pointer
+    3. Custom CLass (without inheritance)
+*/
+
+int main()
+{
+    // We use the BehaviorTreeFactory to register our custom nodes
+    BehaviorTreeFactory factory;
+
+    // The recommended way to create a Node is through inheritance
+    factory.registerNodeType<ApproachObject>("ApproachObject");
+
+    // Registering a SimpleActionNode using a function pointer.
+    // You can use C++11 lambdas or std::bind
+    factory.registerSimpleCondition("CheckBattery", [&](TreeNode &)
+                                    { return CheckBattery(); });
+    // factory.registerSimpleCondition("CheckBattery", std::bind(CheckBattery));
+
+    // You can also create SimpleActionNodes using methods of a class
+    TaskInterface task;
+    factory.registerSimpleAction("StartTask", [&](TreeNode &)
+                                 { return task.start(); });
+    factory.registerSimpleAction("CloseTask", [&](TreeNode &)
+                                 { return task.close(); });
+
+    // Trees are created at run-time
+    auto tree = factory.createTreeFromFile("./../basic_tree.xml");
+
+    // To "execute" a Tree you need to "tick" it.
+    // The tick is propagated to the children based on the logic of the tree.
+    tree.tickWhileRunning();
+
+    return 0;
+}
---
+++ my_nodes.h
@@ -0,0 +1,54 @@
+namespace MyNodes
+{
+    // #1st method: create our own action node
+    // custom sync action node without ports
+    class ApproachObject : public BT::SyncActionNode
+    {
+    public:
+        ApproachObject(const std::string &name) : BT::SyncActionNode(name, {})
+        {
+        }
+
+        // Note: we MUST override the virtual function tick()
+        BT::NodeStatus tick() override
+        {
+            /*
+            The method tick() is the place where the actual Action takes place!!
+            It MUST always return a NodeStatus, i.e. RUNNING, SUCCESS or FAILURE.
+            */
+            std::cout << "ApproachObject: " << this->name() << std::endl;
+            return BT::NodeStatus::SUCCESS;
+        }
+    };
+
+    // #2nd method: Simple function that return a NodeStatus
+    BT::NodeStatus CheckBattery()
+    {
+        std::cout << "[ Battery: OK ]" << std::endl;
+        return BT::NodeStatus::SUCCESS;
+    }
+
+    // #3rd method: We want to wrap into an ActionNode the methods open() and close()
+    class TaskInterface
+    {
+    public:
+        TaskInterface() : _start(true) {}
+
+        BT::NodeStatus start()
+        {
+            _start = true;
+            std::cout << "TaskInterface::start" << std::endl;
+            return BT::NodeStatus::SUCCESS;
+        }
+
+        BT::NodeStatus close()
+        {
+            std::cout << "TaskInterface::close" << std::endl;
+            _start = false;
+            return BT::NodeStatus::SUCCESS;
+        }
+
+    private:
+        bool _start; // shared information
+    };
+}
